5e70749164516419e1c40e11d67c93281c820999
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-07-10 09:04:11 +0200

    Initial commit

---
+++ src/main/java/vincenzomola/u5w2test/U5w2testApplication.java
@@ -0,0 +1,13 @@
+package vincenzomola.u5w2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w2testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w2testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vincenzomola/u5w2test/U5w2testApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzomola.u5w2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w2testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
