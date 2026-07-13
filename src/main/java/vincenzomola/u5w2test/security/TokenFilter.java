23c0ae1429fbd85fbd974b40e5f43eda7abf75c5
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-07-13 17:49:02 +0200

    ok

--- src/main/java/vincenzomola/u5w2test/security/TokenFilter.java
+++ src/main/java/vincenzomola/u5w2test/security/TokenFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.UnavailableException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -13,18 +14,31 @@ import java.io.IOException;
 @Component
 public class TokenFilter extends OncePerRequestFilter {
 
+    private final JWTTools jwtTools;
+
+    public TokenFilter(JWTTools jwtTools) {
+        this.jwtTools = jwtTools;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
+
         String authHeader = request.getHeader("Authorization");
 
-        filterChain.doFilter(request, response);
         if (authHeader == null || !authHeader.startsWith("Bearer "))
             throw new UnavailableException("Inserire il token nell'header");
 
         String accessToken = authHeader.replace("Bearer ", "");
-        
 
+        this.jwtTools.verifyToken(accessToken);
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return new AntPathMatcher().match("/auth/**", request.getServletPath());
     }
 }
